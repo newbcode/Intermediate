828d6e7b593b6e427ac3f0dd9b03085ee65ec4a3
Yunchang Kang <codenewb@gmail.com>
2013-08-29 01:30:40 +0900

    insert display add

--- p_linked.pl
+++ p_linked.pl
@@ -7,25 +7,52 @@ use Sysadm::Install qw (:all);
 use Data::Printer;
 
 my $list;
+my $node = 0;
 my $tail =\$list;
 
-pick("Linked List Operations", ["Insert", "Display", "Size", "Delete",
-        "Addall"], 1);
+while (1) {
+    my $op_mode = pick("Linked List Operations", ["Insert", "Display", "Size",
+            "Delete", "Addall", "Exit"], 1);
 
-if ( $pick eq "Insert" ) {
-    say $pick;
+    if ( $op_mode eq "Insert" ) {
+        say "Enter the number insert : ";
+        chomp (my $num = <STDIN>);
+        insert($num);
+    }
+    elsif ( $op_mode eq "Display" ) {
+        say "Display Element of Hash";
+        display();
+    }
+    elsif ( $op_mode eq "Exit" ) {
+        exit;
+    }
 }
 
 sub insert {
     my $num = shift;
 
-    if ( $list eq undef ) {
-        say "Undef";
-        my $node = { data => $num };
-        my $list = $node;
-        say $list->{data};
+    if ( $node == 0 ) {
+        say "Empity Hash!";
+        $node = { data => $num };
+        $$tail = $node;
+        $tail = \$node->{data};
+        say "$num Insert!";
+    }
+    else {
+        $node = { data => $num };
+        $tail = $node;
+        say "Success insert $tail->{data}";
+        $tail = \$node->{data};
     }
 }
+
+sub display {
+    while ( $list != ) {
+        say "Display $tail->{data}"
+    }
+}
+    
+
 
 =pod
 foreach my $num ( 1..5 ) {
